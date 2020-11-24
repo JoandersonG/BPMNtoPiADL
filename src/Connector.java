eb243fe26e31b07849ff61fd53886ab211c0e7ea
Joanderson <joandersongsantos@yahoo.com>
2020-11-24 15:17:37 -0300

    chore: add getters and setters at Connector

--- src/Connector.java
+++ src/Connector.java
@@ -9,6 +9,30 @@ public class Connector {
         this.toId = toId;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFromId() {
+        return fromId;
+    }
+
+    public void setFromId(String fromId) {
+        this.fromId = fromId;
+    }
+
+    public String getToId() {
+        return toId;
+    }
+
+    public void setToId(String toId) {
+        this.toId = toId;
+    }
+
     @Override
     public String toString() {
         return "Id:" + id + "    From: " + fromId + "    To: " + toId;
