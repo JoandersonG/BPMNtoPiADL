f0ad15b4eac37c55839964faabb443f7773c1b14
Joanderson <joandersongsantos@yahoo.com>
2020-12-27 16:02:06 -0300

    refactor(Gateway): change incoming/outgoing attribute type

--- src/Gateway.java
+++ src/Gateway.java
@@ -3,15 +3,31 @@ import java.util.ArrayList;
 public class Gateway extends Component {
     public enum Type { EXCLUSIVE_GATEWAY, PARALLEL_GATEWAY }
 
-    private ArrayList<Connector> incomings;
-    private ArrayList<Connector> outgoings;
+    private ArrayList<String> incomings;
+    private ArrayList<String> outgoings;
     private Type type;
 
-    public Gateway(String name, String id, ArrayList<Connector> incomings, ArrayList<Connector> outgoings, Type type) {
+    public Gateway(String name, String id, ArrayList<String> incomings, ArrayList<String> outgoings, Type type) {
         super(name, id);
         this.incomings = incomings;
         this.outgoings = outgoings;
         this.type = type;
+    }
+
+    public ArrayList<String> getIncomings() {
+        return incomings;
+    }
+
+    public void setIncomings(ArrayList<String> incomings) {
+        this.incomings = incomings;
+    }
+
+    public ArrayList<String> getOutgoings() {
+        return outgoings;
+    }
+
+    public void setOutgoings(ArrayList<String> outgoings) {
+        this.outgoings = outgoings;
     }
 
     @Override
@@ -19,12 +35,12 @@ public class Gateway extends Component {
         StringBuilder s = new StringBuilder();
         s.append("    Name: '").append(getName()).append("'    Type: ").append(type).append("    id: ").append(getId());
         s.append("\n\t    Incoming: ");
-        for (Connector inc : incomings) {
-            s.append(inc.getId()).append("\t");
+        for (String inc : incomings) {
+            s.append(inc).append("\t");
         }
         s.append("\n\t    Outgoing: ");
-        for (Connector out : outgoings) {
-            s.append(out.getId()).append("\t");
+        for (String out : outgoings) {
+            s.append(out).append("\t");
         }
         s.append("\n");
         return s.toString();
